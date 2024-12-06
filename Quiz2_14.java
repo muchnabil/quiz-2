32ea9aa56646c9eba9c11dc3903e2a08bb191f2f
muchnabil <muchammadnabil@gmail.com>
2024-12-06 09:51:51 +0700

    s 2

--- Quiz2_14.java
+++ Quiz2_14.java
@@ -1,29 +1,36 @@
-import java.util.Scanner;
 public class Quiz2_14 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); 
         String[] books = {"Database", "Mathematics", "Algorithm"};
-        int[][] stock = {
-                {20, 4, 1},
-                {40, 7, 0}, 
-                {50, 20, 10}
+       int[][] stock = {
+               {20, 4, 1},
+               {40, 7, 0}, 
+               {50, 20, 10}
         };
-        System.out.println("Book\t\tIn Stock\tDamaged Stock\tOut-of-stock");
-        for (int i = 0; i < stock.length; i++) {
-            System.out.println(books[i] + "\t\t" + stock[i][0] + "\t\t" + stock[i][1] + "\t\t" + stock[i][2]);
-        }
-        int i = 0;
-        i += 20;
-        i -= 4;
-        i -= 1;
-        i += 40;
-        i -= 7;
-        i += 50;
-        i -= 20;
-        i -= 10;
-        System.out.println("Total book in the library can still borrowed : " + i);
-        System.out.println("The book with largest number can be borrowed is : " + books[2]);
-        System.out.println("Name of book that are heavily borrowed : " + books[0]);
-    }
+       System.out.println("Book\t\tIn Stock\tDamaged Stock\tOut-of-stock");
+       for (int i = 0; i < stock.length; i++) {
+           System.out.println(books[i] + "\t\t" + stock[i][0] + "\t\t" + stock[i][1] + "\t\t" + stock[i][2]);
+         }
+       int totalBorrowable = 0;
+       for (int i = 0; i < stock.length; i++) {
+               totalBorrowable += stock[i][0]; 
+           }
+       System.out.println("\nTotal books that can still be borrowed: " + totalBorrowable);
+       int maxBorrowable = 0;
+       String maxBook = "";
+       for (int i = 0; i < stock.length; i++) {
+           if (stock[i][0] > maxBorrowable) {
+               maxBorrowable = stock[i][0];
+               maxBook = books[i];
+           }
+       }
+       System.out.println("The book with the largest number of books that can be borrowed is: " + maxBook);
+       System.out.println("\nBooks that are heavily borrowed:");
+       for (int i = 0; i < stock.length; i++) {
+           if (stock[i][0] > 30) { 
+               System.out.println(books[i]);
+           }
+       }
+   }
+   
+   
 }
-       
